b01b17843454312fc410ff6c0411c9d4c4a09bda
Salvador Frisco <salvadorfrisco70@gmail.com>
2025-07-19 15:21:17 -0300

    inclusão de path no access control

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,7 +3,7 @@ export async function OPTIONS() {
     status: 204,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Methods": "GET,PATCH,POST,PUT,DELETE,OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
   });
